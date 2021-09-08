9d8803501843acb2ff2c3e1aba8c5db34a79711b
Dagrún Eir Ásgeirsdóttir <daas@itu.dk>
2021-09-08 19:03:22 +0200

    Complete refactoring of file structure

--- LeapYear/LeapYear.cs
+++ LeapYear/LeapYear.cs
@@ -4,6 +4,19 @@ namespace Assignment00
 {
     public class LeapYear
     {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter year:");
+            int year = Int32.Parse(Console.ReadLine());
+            LeapYear ly = new LeapYear();
+            bool leap = ly.IsLeapYear(year);
+            if (leap){
+                Console.WriteLine("yay");
+            } else {
+                Console.WriteLine("nay");
+            }
+        }
+
         public bool IsLeapYear(int year)
         {
             if (year%4 == 0)
